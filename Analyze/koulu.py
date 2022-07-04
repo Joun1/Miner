05c03ef67ff47418d152676a2e8d7b9d754c1fd3
Joun1 <107753652+Joun1@users.noreply.github.com>
2022-07-04 22:41:08 +0300

    Add files via upload

--- Analyze/koulu.py
+++ Analyze/koulu.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
 from sklearn.linear_model import LinearRegression
+import re
 
 #path = r'C:\Users\Jouni Kinnunen\Documents\Python Scripts\Master data_työversio 1_modified.csv'
-path = r'C:\Users\Jouni Kinnunen\Documents\Python Scripts\Master data_työversio_din_iso_modified_modified.csv'
+path = r'E:\koodit\Git\Master data_työversio_din_iso_modified.csv'
 #col_list = ["Syotetyt","Luetut"]
 data = pd.read_csv(path)
 data['Syotetyt mean'] = [data['Syotetyt'].mean() for i in range(len(data))]
@@ -22,7 +22,8 @@ data.plot(kind='line',color='red',y='Syotetyt',grid=True,linewidth=1,ax=ax)
 
 ax1 = fig.add_subplot(211)
 ax2 = fig.add_subplot(212)
-#ax3 = fig.add_subplot(221)
+#ax3 = fig.add_subplot(213)
+#ax4 = fig.add_subplot(214)
 
 data.plot(color=['red','green'],y=['Syotetyt','Syotetyt mean'],grid=True,ax=ax1)
 
@@ -37,7 +38,7 @@ lista ={'col_1':['mean','std','min','max'],
 df = pd.DataFrame.from_dict(lista)
 ex = df.plot(kind='bar',x='col_1',color=['red','blue'])
 for p in ex.patches:
-    ex.annotate(s=np.round(p.get_height(), decimals=2),
+    ex.annotate(text=np.round(p.get_height(), decimals=2),
                 xy=(p.get_x()+p.get_width()/2., p.get_height()),
                 ha='center',
                 va='center',
@@ -50,16 +51,16 @@ plt.figure()
 
 
 
-X = data[["Luetut"]]
-y = data[["Syotetyt"]]
+x = data[["Luetut"]].values
+y = data[["Syotetyt"]].values
 
 regressor = LinearRegression()
-regressor.fit(X, y)
+regressor.fit(x, y)
 
-y_pred = regressor.predict(X)
-
-plt.scatter(X, y, color = 'red')
-plt.plot(X, regressor.predict(X), color = 'green')
+y_pred = regressor.predict(x)
+col =['red','green']
+plt.scatter(x, y, c='red')
+plt.plot(x, y_pred,c='green')
 plt.title('Luetut vs Syotetyt')
 plt.xlabel('Luetut')
 plt.ylabel('Syotetyt')
@@ -67,22 +68,52 @@ plt.ylabel('Syotetyt')
 print("Luetut",data['Luetut'].describe())
 print("Syotetyt",data['Syotetyt'].describe())
 plt.figure()
+plt.subplot(2, 1, 1)
 
+data = pd.read_csv(path)
 data['Std match'] = ''
 for i in range(len(data)):
         x = data['Tekstinsyotto'][i]
-        if x[0:3] == data['Orig standard'][i]:
-                data['Std match'][i] = 'ok'
+        x = re.search(x[0:3],data['Orig standard'][i])
+        if bool(x):
+                if x[0] == 'ANS':
+                        data['Std match'][i] = 'ANSI'
+                else:
+                        data['Std match'][i] = 'ok'
         else:
                 data['Std match'][i] = 'mismatch'
-
-#x = data['Std match'].value_counts().['ok']
-#y = data['Std match'].value_counts().['mismatch']
+#data.to_csv(r'E:\koodit\Git\data.csv')
 x = data['Std match'].value_counts()
-x.plot(kind='bar',color=['green','red'])
-plt.xticks(rotation = 0)
-plt.title('Onko luodun rivin standardi sama kuin Item description')
-plt.xlabel('mismatch = Ei täsmää')
+ex = x.plot(kind='bar',color=['green','red','grey'])
+for p in ex.patches:
+        ex.annotate(text=np.round(p.get_height(), decimals=2),
+                xy=(p.get_x()+p.get_width()/2., p.get_height()),
+                ha='center',
+                va='center',
+                xytext=(0, 11),
+                textcoords='offset points')
 
+ex.tick_params(axis="x",rotation = 0)
 
+ex.set_title('Onko luodun rivin standardi sama kuin Item description',y=1.05)
+
+plt.subplot(2, 1, 2)
+
+x = data.loc[(data['Std match'] == 'mismatch') & (data['Orig standard'])]
+x = x['Orig standard'].value_counts()
+ex = x.plot(kind='bar')
+
+for p in ex.patches:
+    ex.annotate(text=np.round(p.get_height(), decimals=2),
+                xy=(p.get_x()+p.get_width()/2., p.get_height()),
+                ha='center',
+                va='center',
+                xytext=(0, 11),
+                textcoords='offset points')
+
+ex.set_title('Mismatchien erottelu',y=1.05)
+ex.tick_params(axis="x",rotation = 0)
+
+#fig.subplots_adjust(top=0.9, wspace=0.3, hspace=0.3)
+plt.tight_layout()
 plt.show()
